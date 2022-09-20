9843cc367031cc22df8f2637736e271d8ece8784
Gladys María Toledo González <gladysmtg1999@gmail.com>
2022-09-20 18:13:07 -0400

    Learning about Preferences DataStore

---
+++ app/src/main/java/com/example/preferencesdatastore/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.preferencesdatastore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import androidx.lifecycle.lifecycleScope
+import com.example.preferencesdatastore.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var dataStore: DataStore<Preferences>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dataStore = createDataStore(name = "settings")
+
+        binding.btnSave.setOnClickListener {
+            lifecycleScope.launch {
+                save(
+                    binding.etSaveKey.text.toString(),
+                    binding.etSaveValue.text.toString()
+                )
+            }
+        }
+
+        binding.btnRead.setOnClickListener {
+            lifecycleScope.launch {
+                val value = read(binding.etReadKey.text.toString())
+                binding.tvReadValue.text = value ?: "Null value found"
+            }
+        }
+    }
+
+    private suspend fun save(key: String, value: String) {
+        val dataStoreKey = preferencesKey<String>(key)
+        dataStore.edit { settings ->
+            settings[dataStoreKey] = value
+        }
+    }
+
+    private suspend fun read(key: String): String? {
+        val dataStoreKey = preferencesKey<String>(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey]
+    }
+}
